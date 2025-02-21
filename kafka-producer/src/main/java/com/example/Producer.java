5908c8718089b3cdfe110fe4bb77033032c15cc9
talk2viren <virendra.yadav@outlook.com>
2025-02-21 14:01:23 -0800

    draft

--- kafka-producer/src/main/java/com/example/Producer.java
+++ kafka-producer/src/main/java/com/example/Producer.java
@@ -1,10 +1,22 @@
 package com.example;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class Producer {
+
+    @Bean
+    public NewTopic newTopic(){
+        return TopicBuilder.name("new-topic")
+                .partitions(1)
+                .replicas(1)
+                .compact()
+                .build();
+    }
     public static void main(String[] args) {
         SpringApplication.run(Producer.class, args);
     }
